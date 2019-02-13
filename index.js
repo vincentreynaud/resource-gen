6aa0fd3316005fd4b0386b02002f122c862c2e3d
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-02-13 13:58:12 +0100

    try out + add data sample

--- index.js
+++ index.js
@@ -8,29 +8,17 @@ const markdownMagic = require("markdown-magic"); // https://www.npmjs.com/packag
 
 const file = "./Getting Started.webloc";
 
+console.log(path.basename(file));
+
 try {
   fs.readFile(file, { encoding: "utf8" }, function(err, data) {
     const link = data.split("<string>")[1].split("</string>")[0];
     console.log("link:", link);
   });
-  fs.stat(file, (err, stats) => {
-    if (err) throw err;
-    console.log("stats:", stats);
-  });
+  // fs.stat(file, (err, stats) => {
+  //   if (err) throw err;
+  //   console.log("stats:", stats);
+  // });
 } catch (err) {
   console.error(err);
 }
-
-// fs.open("bootstrap.webloc", "r", (err, fd) => {
-//   if (err) throw err;
-//   console.log("file opened");
-//   fs.close(fd, err => {
-//     if (err) throw err;
-//     console.log("file closing");
-//   });
-// });
-
-// fs.stat("Getting Started.webloc", (err, stats) => {
-//   if (err) throw err;
-//   console.log("stats:", stats);
-// });
