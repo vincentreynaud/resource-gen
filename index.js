f8fce58a652b7fe428a955524f0fa5579e663576
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-02-13 16:46:27 +0100

    refractor to sync

--- index.js
+++ index.js
@@ -6,41 +6,34 @@ const markdownMagic = require("markdown-magic");
 const outputFile = "dev-tools-&-resources.md";
 const ignored = ["node_modules", ".git", ".DS_Store"];
 
-const walk = async dirname => {
-  fs.readdir(dirname, (err, files) => {
-    if (err) throw err;
-    let links = [];
+const walk = dirname => {
+  const files = fs.readdirSync(dirname);
+  let links = [];
 
-    files.forEach(file => {
-      file = path.join("./", dirname, file);
+  files.forEach(file => {
+    file = path.join("./", dirname, file);
 
-      // Check for nested dirs
-      fs.stat(file, (err, stat) => {
-        if (err) throw err;
-        if (stat.isDirectory()) {
-          console.log("isDirectory!!!!");
-          walk(file); // give dirname
-        }
-      });
+    // Check for nested dirs
+    const stat = fs.statSync(file);
+    if (stat.isDirectory()) {
+      console.log("isDirectory!!!!");
+      walk(file); // give dirname
+    }
 
-      // Retrieve Link from .webloc files
-      if (path.extname(file) === ".webloc") {
-        fs.readFile(file, { encoding: "utf8" }, (err, content) => {
-          if (err) throw err;
+    // Retrieve Link from .webloc files
+    if (path.extname(file) === ".webloc") {
+      const content = fs.readFileSync(file, { encoding: "utf8" });
 
-          if (content.includes("<string>")) {
-            const link = content.split("<string>")[1].split("</string>")[0];
-            console.log("links before push:", links);
-            links.push(link);
-          } else if (content.includes("SURL_")) {
-            console.log(`FILE ${file} WITH WEIRD ENCORDING HERE`);
-          }
-        });
+      if (content.includes("<string>")) {
+        const link = content.split("<string>")[1].split("</string>")[0];
+        console.log("links before push:", links);
+        links.push(link);
+      } else if (content.includes("SURL_")) {
+        console.log(`FILE ${file} WITH WEIRD ENCORDING HERE`);
       }
-
-      console.log("final links:", links);
-    });
+    }
   });
+  console.log("final links:", links);
 };
 
 try {
