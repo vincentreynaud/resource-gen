1d2998ecd7209e9c4933a965f63c524893b3ec1b
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-02-13 14:43:18 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-const markdownMagic = require("markdown-magic"); // https://www.npmjs.com/package/markdown-magic-directory-tree
+const markdownMagic = require("markdown-magic");
+// https://www.npmjs.com/package/markdown-magic-directory-tree
 
-// const fileURL = new URL(
-//   "file:///Users/vincentreynaud/Dropbox/Development DCI/tools-&-resources"
-// ); // For most fs module functions, the path or filename argument may be passed as a WHATWG URL object
-
-const file = "./data/Getting Started.webloc";
 const outputFile = "dev-tools-&-resources.md";
 const ignored = ["node_modules", ".git", ".DS_Store"];
 
@@ -47,22 +43,22 @@ try {
 }
 
 /* TRY OUTS
-console.log("basename", path.basename(file));
-console.log("delimiter", process.env.PATH.split(path.delimiter));
-console.log("dirname", path.dirname(file));
-console.log("extname", path.extname(file));
+  console.log("basename", path.basename(file));
+  console.log("delimiter", process.env.PATH.split(path.delimiter));
+  console.log("dirname", path.dirname(file));
+  console.log("extname", path.extname(file));
 
-try {
-  fs.readFile(file, { encoding: "utf8" }, function(err, content) {
-    const link = content.split("<string>")[1].split("</string>")[0];
-    console.log("link:", link);
-    return link;
-  });
-  fs.stat(file, (err, stats) => {
-    if (err) throw err;
-    console.log("stats:", stats);
-  });
-} catch (err) {
-  console.error(err);
-}
+  try {
+    fs.readFile(file, { encoding: "utf8" }, function(err, content) {
+      const link = content.split("<string>")[1].split("</string>")[0];
+      console.log("link:", link);
+      return link;
+    });
+    fs.stat(file, (err, stats) => {
+      if (err) throw err;
+      console.log("stats:", stats);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 */
