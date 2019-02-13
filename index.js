f00ea0f6fa339f8970b2270d5508cac9004d9482
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-02-13 14:42:02 +0100

    implement search specifically for webloc files

--- index.js
+++ index.js
@@ -6,19 +6,63 @@ const markdownMagic = require("markdown-magic"); // https://www.npmjs.com/packag
 //   "file:///Users/vincentreynaud/Dropbox/Development DCI/tools-&-resources"
 // ); // For most fs module functions, the path or filename argument may be passed as a WHATWG URL object
 
-const file = "./Getting Started.webloc";
+const file = "./data/Getting Started.webloc";
+const outputFile = "dev-tools-&-resources.md";
+const ignored = ["node_modules", ".git", ".DS_Store"];
 
-console.log(path.basename(file));
+const walk = async dirname => {
+  // implement nested readdir
+  fs.readdir(dirname, (err, files) => {
+    if (err) throw err;
+    console.log("files", files);
+
+    files.forEach(file => {
+      file = path.join("./", dirname, file);
+
+      if (path.extname(file) === "") {
+        console.log("there is a directory here");
+        const nestedDir = path.extname(file);
+        // walk this dir
+      }
+
+      // Retrieve Link from .webloc files
+      if (path.extname(file) === ".webloc") {
+        fs.readFile(file, { encoding: "utf8" }, (err, content) => {
+          if (err) throw err;
+          if (content.includes("<string>")) {
+            const link = content.split("<string>")[1].split("</string>")[0];
+            console.log("link:", link);
+            return link;
+          }
+        });
+      }
+    });
+  });
+};
 
 try {
-  fs.readFile(file, { encoding: "utf8" }, function(err, data) {
-    const link = data.split("<string>")[1].split("</string>")[0];
-    console.log("link:", link);
-  });
-  // fs.stat(file, (err, stats) => {
-  //   if (err) throw err;
-  //   console.log("stats:", stats);
-  // });
+  walk("./data/color");
 } catch (err) {
   console.error(err);
 }
+
+/* TRY OUTS
+console.log("basename", path.basename(file));
+console.log("delimiter", process.env.PATH.split(path.delimiter));
+console.log("dirname", path.dirname(file));
+console.log("extname", path.extname(file));
+
+try {
+  fs.readFile(file, { encoding: "utf8" }, function(err, content) {
+    const link = content.split("<string>")[1].split("</string>")[0];
+    console.log("link:", link);
+    return link;
+  });
+  fs.stat(file, (err, stats) => {
+    if (err) throw err;
+    console.log("stats:", stats);
+  });
+} catch (err) {
+  console.error(err);
+}
+*/
