108df3e182263d98e720279604fc14029490f1e0
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-03-11 17:42:13 +0100

    implement file writing - issues with await to fix

--- cli.js
+++ cli.js
@@ -5,23 +5,29 @@
  * - checkout markdown-magic
  * - Capitalise section titles
  * - order links by alphabetical order? -already done?
- * - make it a CLI npm package to install globally
- * (can use https://github.com/yargs/yargs)
+ * - find how to search outside of project
+ * - make it a CLI npm package to install globally (can use https://github.com/yargs/yargs)
  * - change index.js to cli.js + run $ chmod +x cli.js + npm link
+ * - fill in package.json https://docs.npmjs.com/files/package.json#bin
  *
  * Publishing:
  * - generate .md on github
  * - update every X
  * - host with now
+ *
  */
 
 ("use strict");
 
-const crawl = require("./lib/crawl");
+const print = require("./lib/crawl");
 const [, , dirpath, ...options] = process.argv;
 
+// error handling for no path provided
+
+console.log("process.cwd", process.cwd());
+
 try {
-  crawl(dirpath);
+  print(dirpath);
 } catch (err) {
   console.error(err);
 }
--- dev-tools-&-resources.md
+++ dev-tools-&-resources.md
@@ -1,4 +1,12 @@
-
-  # GRADIENT
-  undefined
-  
+# COLOR 
+https://css-tricks.com/nerds-guide-color-web/ 
+http://www.color-hex.com/ 
+https://color.adobe.com/create/color-wheel/ 
+https://colorable.jxnblk.com/f2ff00/310066 
+https://coolors.co/ 
+https://jxnblk.com/hello-color/?c=6ee603 
+https://jxnblk.com/grays/ 
+https://devdocs.io/javascript/global_objects/string/slice 
+https://palx.jxnblk.com/07c 
+https://monochrome.jxnblk.com/ 
+https://jxnblk.com/shade/?base=%23E1CC41&amp;hueShift=101&amp;saturate=0.43&amp;lighten=-0.02 
