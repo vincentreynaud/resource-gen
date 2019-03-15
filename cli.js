a4f9dd6d50dc155c00e8ffc8684222a42702716c
Vincent <vincentreynaud.de@gmail.com>
2019-03-15 19:51:31 +0100

    add notes + minor adjustment

--- cli.js
+++ cli.js
@@ -2,14 +2,16 @@
 
 /**
  * Improvements:
- * - make debug option to show missing links
  * - implement error handling for no path provided to command
+ * - setup debug option to show missing links
+ * - setup folder depth limit option
  * - order links by alphabetical order? find why capital letters come first..?
- * - find how to search outside of project
+ * - find possible errors in file path handling
+ * - fix html character entity conversion at link retrieval?
+ * - write README.md: installation, supported file types, etc.
  * - make it a CLI npm package to install globally (can use https://github.com/yargs/yargs)
  * - change index.js to cli.js + run $ chmod +x cli.js + npm link
  * - fill in package.json https://docs.npmjs.com/files/package.json#bin
- *
  *
  * Publishing:
  * - generate .md on github
@@ -21,13 +23,12 @@
 "use strict";
 
 const print = require("./lib/print");
-let [, , dirpath, ...options] = process.argv;
-
-// dirpath = "/data/sample"; // debug
+let [, , outputFileName, dirpath, ...options] = process.argv;
 
 // prettier-ignore
+outputFileName = "dev-tools-&-resources" // debug
 dirpath = "/Users/vincentreynaud/Dropbox/Development DCI/tools-&-resources"; // debug
-const outputFile = "dev-tools-&-resources.md";
+const outputFile = `${outputFileName}.md`;
 
 try {
   print(outputFile, dirpath);
