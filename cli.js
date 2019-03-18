e608d7f97d69bd1375537ef9a0cb21e192847c15
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-03-18 11:51:42 +0100

    add notes & minor adjustment

--- cli.js
+++ cli.js
@@ -8,20 +8,22 @@
  * 2. Implement options:
  *   - debug option to show missing links
  *   - folder depth limit option
+ *   - file description option
  *
- * 3. md Rendering:
- *   - write title  of folder with path to it
+ * 3. Write README.md: installation, supported file types, etc.
+ *   - list of unhhandled filepaths
+ *
+ * 4. md Rendering:
+ *   - write title of folder with path to it
  *   - print tabs for each sub level
  *   - order links by alphabetical order? find why capital letters come first..?
  *
- * 4. Error handling:
+ * 5. Error handling:
  *   - for no path provided to command
  *   - find possible errors in file path handling
  *
- * 5. Fixes:
+ * 6. Fixes:
  *  - fix html character entity conversion at link retrieval?
- *
- * 6. Write README.md: installation, supported file types, etc.
  *
  * 7. Make it a CLI npm package to install globally (can use https://github.com/yargs/yargs)
  *  - change index.js to cli.js + run $ chmod +x cli.js + npm link
@@ -37,19 +39,13 @@
 "use strict";
 
 const print = require("./lib/print");
-let [, , outputFileName, dirpath, ...options] = process.argv;
+const [, , outputFileName, dirpath, ...options] = process.argv;
 
 // prettier-ignore
-dirpath = "/Users/vincentreynaud/Dropbox/Development DCI/tools-&-resources"; // debug
-outputFileName = "dev-tools-&-resources"; // debug
+// npm start dev-tools-and-resources /Users/vincentreynaud/Dropbox/Development\ DCI/tools-and-resources
+
 // prettier-ignore
-dirpath = "/Users/vincentreynaud/Desktop/FILES/WORK/2\ Research/4\ Science\ &\ Tech"; // debug
-outputFileName = "research"; // debug
-// prettier-ignore
-// dirpath = "/Users/vincentreynaud/Desktop/FILES/WORK/1\ Production/1\ COLLECTIVE\ ANXIETY/2\ Research/2\ Anthropocène"; // debug
-// prettier-ignore
-dirpath = "/Users/vincentreynaud/Desktop/FILES/WORK/1\ Production/1\ COLLECTIVE\ ANXIETY/2\ Research/"; // debug
-dirpath = "/Users/vincentreynaud/Dropbox/Development DCI/tools-&-resources";
+// dirpath = "/Users/vincentreynaud/Desktop/FILES/WORK/1\ Production/1\ COLLECTIVE\ ANXIETY/2\ Research/";
 
 const outputFile = `output/${outputFileName}.md`;
 
