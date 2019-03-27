a2836924f47bc51d3602295768faa752068f8e3c
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-03-27 14:23:36 +0100

    refractor cli.js

--- cli.js
+++ cli.js
@@ -5,8 +5,7 @@
 const argv = require("yargs")
   .usage("Usage: $0 <output-file> <directory-path> [options]")
   .example(
-    // prettier-ignore
-    "$0 dev-tools-and-resources /Users/vincentreynaud/Dropbox/Development\ DCI/tools-and-resources --depth 5 --ignore snippets"
+    "$0 tools-and-resources /Users/myusername/Development/tools-and-resources --depth 3 --ignore 'code-snippets'"
   )
   .command({
     command: "<output-file> <directory-path> [options]",
@@ -22,7 +21,7 @@ const argv = require("yargs")
   .option("ignore", {
     type: "array",
     alias: "i",
-    describe: "Ignore folders in the base and sub-directories",
+    describe: "Ignore folders in the base- and sub-directories",
     default: []
   })
   .option("description", {
@@ -30,19 +29,21 @@ const argv = require("yargs")
     alias: "t",
     describe: "Describe the generated resource"
   })
-  .demandCommand(2, "Please provide at least 2 arguments")
+  .demandCommand(2, "Please provide least 2 arguments: <directory-path> <output-file>")
   .help().argv;
 // .boolean('v') if flag used returns true
 
-const outputFileName = argv._[0];
-const dirpath = argv._[1];
+// change arguments order
+
+const dirpath = argv._[0];
+const outputFileName = argv._[1];
 const options = {};
 options.depth = argv.depth;
 options.ignore = argv.ignore;
 options.description = argv.description;
 
 // prettier-ignore
-// Run command: npm run gen -- dev-tools-and-resources /Users/vincentreynaud/Dropbox/Development\ DCI/tools-and-resources --i _snippets liked -t "Web Development Bookmarks"
+// Run command: npm run gen -- "/Users/vincentreynaud/Dropbox/Development\ DCI/tools-and-resources" dev-tools-and-resources --i _snippets liked -t "Web Development Bookmarks"
 
 const print = require("./lib/print");
 const outputFile = `output/${outputFileName}.md`;
